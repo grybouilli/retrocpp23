23b98d6622bf09dab306ca41b690e7018a425ad7
Nicolas Gry <nicolas.gry@madic.com>
2025-10-28 11:47:47 +0100

    [fix] fmt : format only depends on optional for tests; falling back on og repo

--- cmake/retrocpp23_fmt.cmake
+++ cmake/retrocpp23_fmt.cmake
@@ -4,15 +4,16 @@ add_library(retrocpp23_fmt INTERFACE)
 add_library(retrocpp23::fmt ALIAS retrocpp23_fmt)
 
 if(CMAKE_CXX_STANDARD LESS 23)
-
+    set(FMT_TEST OFF)
+    set(FMT_INSTALL OFF)
     FetchContent_Declare(
         fmt
-        GIT_REPOSITORY https://github.com/grybouilli/fmt
-        GIT_TAG ea5cf3220c36e6df490a493a02e10b8876217778
+        GIT_REPOSITORY https://github.com/fmtlib/fmt
+        GIT_TAG 12.0.0
     )
     FetchContent_MakeAvailable(fmt)
 
     target_link_libraries(retrocpp23_fmt INTERFACE fmt::fmt)
 else()
-    message("Using std::expected")
+    message("Using std::format")
 endif()
