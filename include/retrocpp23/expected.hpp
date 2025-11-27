b720ebcf46fc6ac8567882674f580d4b583625f6
Nicolas Gry <nicolas.gry@madic.com>
2025-11-27 15:21:54 +0100

    [fix] expected : use compiler preprocessor macro ratherthan custom macro for std::expected use check

--- include/retrocpp23/expected.hpp
+++ include/retrocpp23/expected.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#ifndef USE_TL_EXPECTED  // std::unexpected defined in C++23
+#if __GNUC__ >= 12 || __clang_major__ >= 16  // std::unexpected defined in C++23
 #pragma message("Using std::expected")
 #include <expected>
 #else
